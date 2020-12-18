eaaa11104b206b2c6cbb777fc43ccbbc669fd8dc
Ithonny <thonny02@mail.ru>
2020-12-19 00:42:58 +0300

    add-15

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
@@ -10,20 +11,7 @@ const Profile = () => {
             <div>
                 ava + desc
             </div>
-            <div>
-                my posts
-            <div>
-                new post
-            </div>
-                <div className={s.posts}>
-                    <div className={s.item}>
-                        post 1
-                    </div>
-                    <div className={s.item}>
-                        post 2
-                    </div>
-                </div>
-            </div>
+            <MyPosts />
         </div>
     )
 }
