1ef0dc5c53d457ede897a5a8090ca3f261cb0f33
Ithonny <thonny02@mail.ru>
2021-01-07 02:08:27 +0300

    react-oop

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,38 +1,57 @@
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Vayy', likesCount: 25},
-            {id: 2, message: 'Okey, frend', likesCount: 29},
-            {id: 3, message: 'dasdadsaad', likesCount: 29},
-            {id: 4, message: 'Ellooo', likesCount: 29},
-            {id: 5, message: 'Sypers', likesCount: 29},
-        ],
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Vayy', likesCount: 25},
+                {id: 2, message: 'Okey, frend', likesCount: 29},
+                {id: 3, message: 'dasdadsaad', likesCount: 29},
+                {id: 4, message: 'Ellooo', likesCount: 29},
+                {id: 5, message: 'Sypers', likesCount: 29},
+            ],
+            newPostText: 'it-thonny.com'
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrey'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Vasiliy'},
+                {id: 6, name: 'Oleg'}
+            ],
+            messages: [
+                {id: 1, message: 'HI'},
+                {id: 2, message: 'How is your'},
+                {id: 3, message: 'YOYO'},
+                {id: 4, message: 'YOYO'},
+            ],
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Vasiliy'},
-            {id: 6, name: 'Oleg'}
-        ],
-        messages: [
-            {id: 1, message: 'HI'},
-            {id: 2, message: 'How is your'},
-            {id: 3, message: 'YOYO'},
-            {id: 4, message: 'YOYO'},
-        ],
-    }
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('State changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 34,
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewsPostText(newText) {
+            this._state.profilePage.newPostText = newText;
+            this._callSubscriber(this._state);
+        },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
 }
 
-export let addPost = (postMessage) => {
-    let newPost = {
-        id: 5,
-        message: postMessage,
-        likesCount: 34,
-    }
-    state.profilePage.posts.push(newPost);
-}
+export default store;
+window.store = store;
 
-export default state;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import reportWebVitals from './reportWebVitals';
+import store from "./Redux/state";
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
-import reportWebVitals from './reportWebVitals';
-import Post from "./components/Profile/MyPosts/Post/Post";
-import state from "./Redux/state";
-import {addPost} from "./Redux/state";
 import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App state={state} addPost={addPost}/>
-    </BrowserRouter>, document.getElementById('root'));
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={store.addPost.bind(store)}
+                 updateNewsPostText={store.updateNewsPostText.bind(store)}/>
+        </BrowserRouter>, document.getElementById('root'));
+}
 
+
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
